9a07d1b50af16e90f0f74d88dbcdbfdc87fb867f
Guy Palmer <guypalmer@Guys-Air.attlocal.net>
2018-04-20 14:07:07 -0500

    finished bamazonCustomer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,54 +10,74 @@ var connection = mysql.createConnection({
     database: "bamazonDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         throw err;
-    }
-    else {
+    } else {
         queryAllProducts();
         promptUser();
     }
 })
 
 function queryAllProducts() {
-    connection.query("SELECT id, product_name, price FROM products", function(err, res) {
+    connection.query("SELECT id, product_name, price FROM products", function (err, res) {
         if (err) throw err;
-            console.table("\nOUR PRODUCTS", res);
+        console.table("\nOUR PRODUCTS", res);
     })
 }
 
 function promptUser() {
     inquirer.prompt([
-        {
-            name: "confirm",
-            message: "Would you like to make a purchase today?\n",
-            default: true
-        },
-        {
-            name: "id",
-            message: "What item would you like to Purchase?\n" 
-        },
-        {
-            name: "quantity",
-            message: "How many units would you like to buy?\n" 
-        },
-        {
-            type: "confirm",
-            name: "confirm",
-            message: "Would you like to confirm this purchase?\n"
-        }
-    ]).then(function checkStore(answers) {
-        //check database to see if there is enough product
-        //if there is, fulfill customers order and log cost of purchase
-        //if not, log we dont have enough in stock and prevent order from continuing.
-    }).then(function () {
-        endConnection();
-    }).catch(function(err) {
-        if (err) throw err;
-    })
+            {
+                name: "id",
+                message: "What item would you like to purchase today?\n"
+            },
+            {
+                name: "quantity",
+                message: "How many units would you like to purchase?\n"
+            },
+        ]).then(function checkStore(answers) {
+            for (var i = 0; i < res.length; i++) {
+                if (parseInt(ansers.id) === res[i].id) {
+                    if (res[i].quanitiy >= (parseInt(answers.quantity))) {
+                        console.log("\nThank you for your order.");
+
+                        var purchaseCost = parseFloat(res[i].price * answers.quantity);
+                        console.table("\nYour total is: $" + purchaseCost.toFixed(2));
+
+                        var newQuantity = res[i].quantity - (parseInt(answers.quantity));
+
+                        connection.query("UPDATE products SET ? Where ?", [{
+                            quntity: newQuantity
+                        }, {
+                            id: answers.id
+                        }], function (err, res) {
+                            if (err) console.log(err);
+                            else {
+                                printUpdatedProducts();
+                            }
+                        })
+                    } else {
+                        console.log("\nI'm sorry, we dont enough in stock, but will be recieving a new shipment soon!\n");
+                        promptUser(res);
+                    }
+                }
+            }
+        })
+        .catch(function (err) {
+            if (err) {
+                console.log(err);
+            }
+        })
 }
 
-function endConnection() {
+function printUpdatedProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if(err) throw err;
+        console.log("");
+        for (var i = 0; i < res.length; i++) {
+            console.table(res[i].product_name + " | Quantity left: " + res[i].quantity);
+        }
+    })
     connection.end();
 }
